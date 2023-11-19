698df77c34eeb50032f92f184f9235c48e97c3f2
Your Name <you@example.com>
2023-11-19 17:10:55 +0100

    letzte änderung

--- afterDocker/my-backend/server.js
+++ afterDocker/my-backend/server.js
@@ -1,33 +1,60 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
-
-const port = 3001;
+const mysql = require('mysql2/promise');
 const cors = require('cors'); // CROSS ORIGIN MIDDLEWARE, to accept  request from ur frontend
+const port = 3001;
 
 
 app.use(cors()); // fürs CORS problem
 require('dotenv').config();  // fürs .env file
+app.use(express.json());
 
 
-const db = mysql.createConnection({ // verbindet mit der Datenbank im netzwerk
+const db = mysql.createPool({ // verbindet mit der Datenbank im netzwerk
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the database');
+
+ // fürs lesen
+ app.get('/api/products', async (req, res) => {
+  const [rows, fields] = await db.execute('SELECT * FROM products');
+  res.json(rows);
 });
 
-app.get('/api/products', (req, res) => { // from table
-  db.query('SELECT * FROM products', (err, results) => {
-    if (err) throw err;
-    res.send(results);
-  });
+
+// zum posten
+app.post('/api/products', async (req, res) => {
+  const { name, description, price, stock } = req.body; // extrahiert name,description,price,stock aus dem body
+  try {
+    const [result] = await db.execute('INSERT INTO products (name, description, price, stock) VALUES (?, ?, ?, ?)', [name, description, price, stock]);
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error beim hinzufügen eines Produktes.' });
+  }
 });
+
+// PUT = update
+app.put('/api/products/:id', async (req, res) => {
+  const { name, description, price, stock } = req.body;
+  const [result] = await db.execute('UPDATE products SET name = ?, description = ?, price = ?, stock = ? WHERE id = ?', [name, description, price, stock, req.params.id]);
+  res.json(result);
+});
+
+// fürst löschen
+app.delete('/api/products/:id', async (req, res) => {
+  const [result] = await db.execute('DELETE FROM products WHERE id = ?', [req.params.id]);
+  res.json(result);
+});
+
+
+
 
 app.listen(port, () => {
   console.log(`Server laeft auf PORT:${port}`);
